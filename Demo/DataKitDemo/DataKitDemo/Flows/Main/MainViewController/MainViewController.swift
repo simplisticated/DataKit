521372f19aad44473a6f94ffe1ce5f6b8bac588d
Igor Matyushkin <igormatyushkin014@gmail.com>
2016-07-01 11:21:41 +0300

    Created demo project

---
+++ Demo/DataKitDemo/DataKitDemo/Flows/Main/MainViewController/MainViewController.swift
@@ -0,0 +1,57 @@
+//
+//  MainViewController.swift
+//  DataKitDemo
+//
+//  Created by Igor Matyushkin on 01.07.16.
+//  Copyright © 2016 Igor Matyushkin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    // MARK: Class variables & properties
+    
+    
+    // MARK: Public class methods
+    
+    
+    // MARK: Private class methods
+    
+    
+    // MARK: Initializers
+    
+    
+    // MARK: Deinitializer
+    
+    deinit {
+    }
+    
+    
+    // MARK: Outlets
+    
+    
+    // MARK: Object variables & properties
+    
+    
+    // MARK: Public object methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Private object methods
+    
+    
+    // MARK: Actions
+    
+    
+    // MARK: Protocol methods
+    
+}
---
+++ Demo/DataKitDemo/DataKitDemoTests/DataKitDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DataKitDemoTests.swift
+//  DataKitDemoTests
+//
+//  Created by Igor Matyushkin on 01.07.16.
+//  Copyright © 2016 Igor Matyushkin. All rights reserved.
+//
+
+import XCTest
+@testable import DataKitDemo
+
+class DataKitDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
